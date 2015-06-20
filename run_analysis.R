ea58cdeb0d6ad7e09d29594cf5699188925cc7cf
yuvrajsingla <yuvraj1990singla@gmail.com>
2015-06-21 02:11:12 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+## run_analysis.R 
+## The goal is to fetch data and process it to prepare tidy data that can be used for later analysis
+
+## Data https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
